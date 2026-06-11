d4b7552a19b667c7f519fcc975d72edd3fcb417e
1-abdelrahman-khalil-1 <abdelrahmanabdelrahman2003@gmail.com>
2026-06-11 12:22:02 +0300

    increase general rate limiter

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ import { getLanguage, t, tr } from "../lib/i18n/index.js";
 
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res) => {
